44859be30453253a04702e739f82e95b121ba584
IceColdDude759 <81350357+IceColdDude759@users.noreply.github.com>
2021-03-29 20:31:18 +0530

    bug fixes

--- Player.py
+++ Player.py
@@ -4,53 +4,51 @@ from spritesheetparser import Spritesheet
 class Player(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.LEFT_KEY, self.RIGHT_KEY, self.FACING_LEFT = False, False, False
-		self.UP_KEY, self.DOWN_KEY, self.FACING_DOWN = False, False, False
-		self.is_jumping, self.on_ground = False, False
-		self.gravity, self.friction = .35, -.09
+		self.LEFT_KEY, self.RIGHT_KEY = False, False
+		self.UP_KEY, self.DOWN_KEY = False, False
+		self.friction = -0.09
 		self.image = Spritesheet('resources/Blockz').get_sprite('white.png')
 		self.image = pygame.transform.scale(self.image, (32,32))
 		self.rect = self.image.get_rect()
 		self.position, self.velocity = pygame.math.Vector2(0,0), pygame.math.Vector2(0,0)
 		self.acceleration = pygame.math.Vector2(5,5)
-		self.left_border = 0
-		self.top_border = 0
-		self.bottom_border = 1280
-		self.right_border = 2400
 		self.max_vel = 8
+		self.bump = False
 
 	def draw(self, display,camera_offset_x,camera_offset_y):
 		display.blit(self.image, (self.rect.x - camera_offset_x, self.rect.y - camera_offset_y))
 
 
 	def update(self, dt, tiles):
-		self.vertical_movement(dt)
-		self.horizontal_movement(dt)
+		
 		#print(self.rect)
 		#print(self.rect.w)
-	
+		self.vertical_movement(dt)
 		self.checkCollisionsy(tiles)
+		
+		self.horizontal_movement(dt)
 		self.checkCollisionsx(tiles)
 
 
 	def horizontal_movement(self,dt):		
 		self.acceleration.x = 0
-		if self.LEFT_KEY:
+		if self.LEFT_KEY and not self.bump:
 			self.acceleration.x -= .6
-		elif self.RIGHT_KEY:
+		elif self.RIGHT_KEY and not self.bump:
 			self.acceleration.x += .6
 		self.acceleration.x += self.velocity.x * self.friction
 		self.velocity.x += self.acceleration.x * dt
 		self.limit_x_velocity(self.max_vel)
 		self.position.x += self.velocity.x * dt + (self.acceleration.x * .5) * (dt * dt)
 		self.rect.x = self.position.x
+		
 
 
 	def vertical_movement(self,dt):
 		self.acceleration.y = 0
-		if self.UP_KEY:
+		if self.UP_KEY and not self.bump:
 			self.acceleration.y -= .6
-		elif self.DOWN_KEY:
+		elif self.DOWN_KEY and not self.bump:
 			self.acceleration.y += .6
 		self.acceleration.y += self.velocity.y * self.friction
 		self.velocity.y += self.acceleration.y * dt
@@ -81,26 +79,34 @@ class Player(pygame.sprite.Sprite):
 
 	def checkCollisionsx(self, tiles):
 		collisions = self.get_hits(tiles)
+		self.bump = False
 		for tile in collisions:
 			if self.velocity.x > 0:  # Hit tile moving right
 				self.position.x = tile.rect.left - self.rect.w
-				self.rect.x = self.position.x
+				self.rect.x = self.position.x 
 				self.velocity.x = 0
+				self.bump = True
 			elif self.velocity.x < 0:  # Hit tile moving left
 				self.position.x = tile.rect.right
-				self.rect.x = self.position.x
+				self.rect.x = self.position.x 
 				self.velocity.x = 0
+				self.bump = True
+		
 			
 
 	def checkCollisionsy(self, tiles):
 		collisions = self.get_hits(tiles)
+		self.bump = False
 		for tile in collisions:
 			if self.velocity.y > 0:  # Hit tile moving down
 				self.position.y = tile.rect.top - self.rect.h
 				self.rect.y = self.position.y
+				self.bump = True 
+				#- 2
 				self.velocity.y = 0
 			elif self.velocity.y < 0:  # Hit tile moving up
 				self.position.y = tile.rect.bottom
-				self.rect.y = self.position.y
+				self.rect.y = self.position.y 
 				self.velocity.y = 0
+				self.bump = True
 		
--- main.py
+++ main.py
@@ -5,82 +5,104 @@ from camera import *
 from Player import Player
 
 
-pygame.init()
 
-## GAME VARIABLE
-screen_width, screen_height = 800, 480
-tile_size = 32
+class Engine():
+	def __init__(self):
+		
+		pygame.init()
+		#pygame.mixer.pre_init(44100, -16, 2, 512)
+		#mixer.init()
+		self.clock = pygame.time.Clock()
+		self.FPS = 60
+		self.running = True
+		self.menu_bol = False
+		self.screen_width, self.screen_height = 800, 480
+		self.tile_size = 32
+		self.flags = pygame.RESIZABLE | pygame.SCALED
+		
+		self.screen = pygame.display.set_mode((self.screen_width, self.screen_height), self.flags)
+		self.title = "A GAME"
+		pygame.display.set_caption(self.title)
 
-running = True
-clock = pygame.time.Clock()
-TARGET_FPS = 60
+		self.main_tiles = Spritesheet('resources/Blockz')
+		self.world = TileMap('map.csv', self.main_tiles)
+		self.left_border = 0
+		self.top_border = 0
+		self.bottom_border = self.world.map_h
+		self.right_border = self.world.map_w
+		self.player = Player()
+		self.camera = Camera(self)
+		self.follow = Follow(self)
+		self.border = Border(self)
+		self.auto = Auto(self)
+		self.camera.setmethod(self.border)
+		self.player.position.x, self.player.position.y = self.world.start_x, self.world.start_y
 
 
-## INITIALIZEING EVERYTHING
-bg_img = pygame.image.load('resources/bg.jpg')
-bg_img = pygame.transform.scale(bg_img, (screen_width,screen_height-tile_size*0))
-display = pygame.display.set_mode(((screen_width,screen_height)))
+	def reset_level(self, level):
+		pass
 
-player=Player()
-
-camera = Camera(player)
-follow = Follow(camera,player)
-border = Border(camera,player)
-auto = Auto(camera,player)
-camera.setmethod(follow)
-
-spritesheet = Spritesheet('resources/Blockz')
-map = TileMap('map.csv', spritesheet )
-player.position.x, player.position.y = map.start_x, map.start_y
+	
+	def menu(self):
+		pass
 
 
+	def input(self):
+		for event in pygame.event.get():
 
-while running:
-	dt =clock.tick(60) * .001 * TARGET_FPS 
+			if event.type == pygame.QUIT:
+				self.running = False
 
-	## PROCESS INPUT
-	for event in pygame.event.get():
+				## PROCESS KEYPRESS
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					self.player.LEFT_KEY = True
+				elif event.key == pygame.K_RIGHT:
+					self.player.RIGHT_KEY = True
+				elif event.key == pygame.K_UP:
+					self.player.UP_KEY = True
+				elif event.key == pygame.K_DOWN:
+					self.player.DOWN_KEY = True
 
-		if event.type == pygame.QUIT:
-			running = False
+				## HANDEL CAMERA MOVEMENT	
+				elif event.key == pygame.K_1:
+					self.camera.setmethod(self.follow)
+				elif event.key == pygame.K_2:
+					self.camera.setmethod(self.auto)
+				elif event.key == pygame.K_3:
+					self.camera.setmethod(self.border)
 
-			## PROCESS KEYPRESS
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				player.LEFT_KEY = True
-			elif event.key == pygame.K_RIGHT:
-				player.RIGHT_KEY = True
-			elif event.key == pygame.K_UP:
-				player.UP_KEY = True
-			elif event.key == pygame.K_DOWN:
-				player.DOWN_KEY = True
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT:
+					self.player.LEFT_KEY = False
+				elif event.key == pygame.K_RIGHT:
+					self.player.RIGHT_KEY = False
+				elif event.key == pygame.K_UP:
+					self.player.UP_KEY = False
+				elif event.key == pygame.K_DOWN:
+					self.player.DOWN_KEY = False
 
-			## HANDEL CAMERA MOVEMENT	
-			elif event.key == pygame.K_1:
-				camera.setmethod(follow)
-			elif event.key == pygame.K_2:
-				camera.setmethod(auto)
-			elif event.key == pygame.K_3:
-				camera.setmethod(border)
+	def update(self):
+		dt = self.clock.tick(60) * .001 * self.FPS 
+		self.player.update(dt,self.world.tiles)
+		self.camera.scroll()
 
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_LEFT:
-				player.LEFT_KEY = False
-			elif event.key == pygame.K_RIGHT:
-				player.RIGHT_KEY = False
-			elif event.key == pygame.K_UP:
-				player.UP_KEY = False
-			elif event.key == pygame.K_DOWN:
-				player.DOWN_KEY = False
+	def draw(self):
+		self.screen.fill((0,200,240))
+		self.world.draw_map(self.screen,self.camera.offset.x,  self.camera.offset.y)
+		self.player.draw(self.screen,self.camera.offset.x,  self.camera.offset.y)
+		#pygame.draw.rect(self.screen, (255, 0, 0), self.player.rect, 2)
+		pygame.display.update()
 
-	## SPRITE UPDATE
-	player.update(dt,map.tiles)
-	camera.scroll()
 
-	## DRAW SCREEN
-	display.fill((0,0,0))
-	#display.blit(bg_img, (0, 0))
-	map.draw_map(display, camera.offset.x, camera.offset.y)
-	player.draw(display, camera.offset.x, camera.offset.y)
-	#pygame.draw.rect(canvas, (255, 0, 0), player.rect, 2)
-	pygame.display.update()
+
+
+
+
+
+if __name__=="__main__":
+	engine = Engine()
+	while engine.running :
+		engine.input()
+		engine.update()
+		engine.draw()	
