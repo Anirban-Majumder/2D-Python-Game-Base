2368f6fc8a35df41b22eaa9b1566f8a0f3a1419c
IceColdDude759 <81350357+IceColdDude759@users.noreply.github.com>
2021-04-01 14:16:23 +0530

    update

--- menu.py
+++ menu.py
@@ -34,25 +34,42 @@ class Button():
 class Menu():
 	def __init__ (self,engine):
 		self.engine = engine 
-		self.buttons =[]
+		self.buttons = {}
 		self.font = pygame.font.SysFont("Verdana", 70)#, True, True)
 		self.white = (255,255,255)
 		self.black = (0,0,0)
+		self.colour = self.black
 
 	def draw(self):
 		for button in self.buttons :
-			button.draw(self.engine.screen)
+			self.buttons[button][1] = (self.buttons[button][0].draw(self.engine.screen))
 	
 	def draw_text(self, text, colour):
 		img = self.font.render(text, True, colour)
 		return img
 
 	def add(self, text, x, y):
-		self.buttons.append(Button(x, y, self.draw_text(text, self.white)))
+		self.buttons[text] = [Button(x, y, self.draw_text(text, self.colour)), False]
 
 
 class Mainmenu(Menu):
 	def __init__(self, engine):
 		Menu.__init__(self,engine)
-		self.add(Cyka Blayt)
+		self.font = pygame.font.SysFont("Verdana", 30, True, True)
+		self.colour = (170, 255, 255)
+		self.add('A GAME', 320, 50)
+		self.add('Start', 370, 150)
+		self.add('Options',340,250)
+		self.add('Exit', 370, 350)
+
+
+class Deadmenu(Menu):
+	def __init__(self, engine):
+		Menu.__init__(self,engine)
+		self.font = pygame.font.SysFont("Verdana", 30, True, True)
+		self.colour = (170, 255, 255)
+		self.add('Exit', 550, 250)
+		self.add('Restart', 170, 250)
+
+
 
